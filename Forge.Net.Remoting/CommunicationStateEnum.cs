dbfe7ed3b7af697c1d2d5a2ab3a785f07363084e
Zoltan Juhasz <jzo@jzo.hu>
2024-01-17 00:45:51 +0100

    Add files via upload

---
+++ Forge.Net.Remoting/CommunicationStateEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace Forge.Net.Remoting
+{
+
+    /// <summary>Defines the states in which an System.ServiceModel.ICommunicationObject can exist.</summary>
+    public enum CommunicationStateEnum
+    {
+        /// <summary>Indicates that the communication object has been instantiated and is configurable, but not yet open or ready for use.</summary>
+        Created,
+
+        /// <summary>
+        /// Indicates that the communication object is being transitioned from the Forge.Net.Remoting.CommunicationStateEnum.Created state to the Forge.Net.Remoting.CommunicationStateEnum.Opened state.
+        /// </summary>
+        Opening,
+
+        /// <summary>Indicates that the communication object is now open and ready to be used.</summary>
+        Opened,
+
+        /// <summary>Indicates that the communication object is transitioning to the Forge.Net.Remoting.CommunicationStateEnum.Closed state.</summary>
+        Closing,
+
+        /// <summary>Indicates that the communication object has been closed and is no longer usable.</summary>
+        Closed,
+
+        /// <summary>Indicates that the communication object has encountered an error or fault from which it cannot recover and from which it is no longer usable.</summary>
+        Faulted
+    }
+}
