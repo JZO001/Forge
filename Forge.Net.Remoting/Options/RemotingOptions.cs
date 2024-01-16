4b6b17264214304e3765fdbbb2a6971ef16ced86
Zoltan Juhasz <jzo@jzo.hu>
2024-01-17 00:45:32 +0100

    Add files via upload

---
+++ Forge.Net.Remoting/Options/RemotingOptions.cs
@@ -0,0 +1,16 @@
+﻿using Forge.Configuration;
+
+namespace Forge.Net.Remoting.Options
+{
+
+    /// <summary>Remoting options</summary>
+    public class RemotingOptions
+    {
+
+        /// <summary>Gets or sets the remoting configuration.</summary>
+        /// <value>The remoting configuration.</value>
+        public PropertyItem RemotingConfiguration { get; set; }
+
+    }
+
+}
